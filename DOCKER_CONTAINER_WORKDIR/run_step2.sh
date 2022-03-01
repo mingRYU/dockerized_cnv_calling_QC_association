c37b5a6874b9a21d938df38235ec377c21c8f986
Mingrui Yu <myu@wm3d8-84e.broadinstitute.org>
2022-03-01 16:24:36 -0500

    fixed bug in job script

--- DOCKER_CONTAINER_WORKDIR/run_step2.sh
+++ DOCKER_CONTAINER_WORKDIR/run_step2.sh
@@ -9,7 +9,9 @@ export IPNBASE=/software/ipn_0.582
 export PYTHONPATH=$PYTHONPATH:/software/ipn_0.582/ipnlib
 bash ilmn.sh /WORKDIR/ipn/${COHORT_NAME}/data_aux/conf.sublist${subset_code}.txt
 
-## run_ipattern.py of some chr.p or chr.q may failed due to not having enough probes after filtering, so we run following 
-## line to concatenate all .report files under directory ${COHORT_NAME}.sublist*_call:
-
-cat /software/ipn_0.582/preprocess/ilmn/${COHORT_NAME}.sublist${subset_code}_call/*report > /WORKDIR/ipn/${COHORT_NAME}/out/${COHORT_NAME}.sublist${subset_code}_all_calls.txt
+if [ -e /WORKDIR/ipn/${COHORT_NAME}/out/${COHORT_NAME}.sublist${subset_code}_all_calls.txt ]
+then
+    ## run_ipattern.py of some chr.p or chr.q may failed due to not having enough probes after filtering, so we run following 
+    ## line to concatenate all .report files under directory ${COHORT_NAME}.sublist*_call:
+    cat /software/ipn_0.582/preprocess/ilmn/${COHORT_NAME}.sublist${subset_code}_call/*report > /WORKDIR/ipn/${COHORT_NAME}/out/${COHORT_NAME}.sublist${subset_code}_all_calls.txt
+fi
