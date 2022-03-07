c8462428bbc60d52eba06240bcc4b87a72e9b445
mingRYU <myu@cnv-pipeline-testing.c.huang-lab-general.internal>
2022-03-07 15:46:29 +0000

    fixed bug in step.sh

--- DOCKER_CONTAINER_WORKDIR/run_step2.sh
+++ DOCKER_CONTAINER_WORKDIR/run_step2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /WORKDIR/config.sh
 
-subset_code=aa   # <--- change this to "aa", "ab", "ac", "ad" to run this step on different subsets 
+subset_code=aa   # <--- change this to "aa", "ab", "ac", "ad", etc, to run this step on different subsets 
 
 # run iPattern
 cd /software/ipn_0.582/preprocess/ilmn/
@@ -9,7 +9,7 @@ export IPNBASE=/software/ipn_0.582
 export PYTHONPATH=$PYTHONPATH:/software/ipn_0.582/ipnlib
 bash ilmn.sh /WORKDIR/ipn/${COHORT_NAME}/data_aux/conf.sublist${subset_code}.txt
 
-if [ -e /WORKDIR/ipn/${COHORT_NAME}/out/${COHORT_NAME}.sublist${subset_code}_all_calls.txt ]
+if [ ! -f /WORKDIR/ipn/${COHORT_NAME}/out/${COHORT_NAME}.sublist${subset_code}_all_calls.txt ]
 then
     ## run_ipattern.py of some chr.p or chr.q may failed due to not having enough probes after filtering, so we run following 
     ## line to concatenate all .report files under directory ${COHORT_NAME}.sublist*_call:
